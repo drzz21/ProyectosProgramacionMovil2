5193df5e61b067fafd6b6f2e1ffcb5a2e082ca42
Francisco Diosdado <drz.y2k@gmail.com>
2018-01-31 21:59:28 -0600

    servicios enlazados

---
+++ MisServiciosAndroidA/app/src/main/java/net/ivanvega/misserviciosandroida/MainActivity.java
@@ -0,0 +1,96 @@
+package net.ivanvega.misserviciosandroida;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ServicioEnlazado mService;
+    boolean mBound = false;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to LocalService
+        Intent intent = new Intent(this, ServicioEnlazado.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        super.onStop();
+        if (mBound) {
+            unbindService(mConnection);
+            mBound = false;
+        }
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnI_click(View v){
+        Toast.makeText(this,"INTERACTUANDO HILO PRINCIPAL",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void btnIS_click(View v){
+        Toast.makeText(this,"arrancando intent servicio",
+                Toast.LENGTH_SHORT).show();
+
+        Intent is = new Intent(this,MiIntentService.class);
+
+        startService(is);
+    }
+
+    public void btnS_click(View v){
+        Toast.makeText(this,"arrancando intent servicio",
+                Toast.LENGTH_SHORT).show();
+
+        Intent is = new Intent(this,HelloService.class);
+
+        startService(is);
+    }
+
+    public void onButtonClick(View v) {
+        if (mBound) {
+            // Call a method from the LocalService.
+            // However, if this call were something that might hang, then this request should
+            // occur in a separate thread to avoid slowing down the activity performance.
+            int num = mService.getRandomNumber();
+            Toast.makeText(this, "number: " + num, Toast.LENGTH_SHORT).show();
+        }
+    }
+    private ServiceConnection mConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            ServicioEnlazado.LocalBinder binder = (ServicioEnlazado.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+
+
+
+}
--- MisServiciosAndroidA/app/src/main/java/net/ivanvega/misserviciosandroida/ServicioEnlazado.java
+++ MisServiciosAndroidA/app/src/main/java/net/ivanvega/misserviciosandroida/ServicioEnlazado.java
@@ -1,8 +1,41 @@
 package net.ivanvega.misserviciosandroida;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import java.util.Random;
+
 /**
  * Created by Dios on 31/ene/2018.
  */
 
-public class ServicioEnlazado {
+public class ServicioEnlazado extends Service {
+
+    private final IBinder mBinder = new LocalBinder();
+    // Random number generator
+    private final Random mGenerator = new Random();
+
+    /**
+     * Class used for the client Binder.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with IPC.
+     */
+    public class LocalBinder extends Binder {
+        ServicioEnlazado getService() {
+            // Return this instance of LocalService so clients can call public methods
+            return ServicioEnlazado.this;
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mBinder;
+    }
+
+    /** method for clients */
+    public int getRandomNumber() {
+        return mGenerator.nextInt(100);
+    }
 }
