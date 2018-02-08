f529d3bad66c8a761f986e4fe4a8ed8c16a7122c
Francisco Diosdado <drz.y2k@gmail.com>
2018-02-07 19:40:37 -0600

    ...

--- MyApplication/app/src/main/java/com/example/gerardo/myapplication/autoinicio.java
+++ MyApplication/app/src/main/java/com/example/gerardo/myapplication/autoinicio.java
@@ -1,8 +1,19 @@
 package com.example.gerardo.myapplication;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
 /**
  * Created by Dios on 07/feb/2018.
  */
 
-public class autoinicio {
+public class autoinicio extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        Intent service = new Intent(context,  Servicio.class);
+        context.startService(service);
+    }
+
 }
